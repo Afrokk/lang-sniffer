f75053a37f12975531ed3bf5b9c0179807ff4a8e
Afrokk <afrasiyabkhan379@gmail.com>
2023-03-01 18:21:45 -0500

    setup logic

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-import {francAll} from 'franc';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import langs from 'langs';
-import path from 'path';
+import { franc } from "franc";
+import { fileURLToPath } from "url";
+import express from "express";
+import langs from "langs";
+import path from "path";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // Base route
-app.get('/', (req, res) => {
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.render("index", { langName: null });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.phrase);
+  const langCode = franc(req.body.phrase);
+  if (langCode === "und") {
+    res.render("index", {
+      langName: " not found. Please make sure the input is longer or try a different phrase",
+    });
+  } else {
+    console.log("Lang code is " + langCode);
+    try {
+      const langName = langs.where("3", langCode).name;
+      console.log(langName);
+      res.render("index", { langName });
+    }
+    catch (err) {
+      res.render("index", {
+        langName: " not found. Please make sure the input is longer or try a different phrase",
+      });
+    };
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
