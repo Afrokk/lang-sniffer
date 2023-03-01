bd35e8f96f5905705a160568a502c8bbc0ec5988
Afrokk <afrasiyabkhan379@gmail.com>
2023-03-01 17:23:52 -0500

    Setup EJS, paths

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-import {franc, francAll} from 'franc';
+import {francAll} from 'franc';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import langs from 'langs';
-
+import path from 'path';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Base route
 app.get('/', (req, res) => {
-  res.send('Welcome to the language sniffer! Work in progress.');
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
